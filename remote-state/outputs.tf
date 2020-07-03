ac8ec664f2569a0ba66b1939497b5bc4c8f6ff2b
Travis <t@tburton.net>
2020-07-03 16:02:18 -0400

    update outputs.tf

--- remote-state/outputs.tf
+++ remote-state/outputs.tf
@@ -1,11 +1,3 @@
-output "account" {
-    value = data.aws_caller_identity.current.account_id
-}
-
 output "s3_bucket_name" {
     value = aws_s3_bucket.tfstate.id
-}
-
-output "region" {
-    value = aws_s3_bucket.tfstate.region
 }
